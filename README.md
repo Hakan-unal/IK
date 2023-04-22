0a68e293e6f05e9638b38f97455fe829fa0cf9c8
Con Sınov <hakanunal_1996@hotmail.com>
2023-04-22 22:46:42 +0300

    updated

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# chatPushNotification
+# zweig-books
--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,60 @@
-import { useEffect } from "react";
-import { Row } from "antd"
-import Main from "./components/layout/main"
 
-const App = () => {
+import { Layout, Menu, theme } from "antd"
+import { Route, Routes } from 'react-router-dom';
+import Home from "./pages/home/home"
+import page404 from "./pages/404/404"
+import Sidebar from './components/layout/sidebar';
+import Demo from "./pages/demo/demo"
+import Components from "./pages/components/components"
+import Portfolio from "./pages/portfolio/portfolio"
+import Projects from "./pages/projects/projects"
+
+const { Content } = Layout;
+
+const App: React.FC = () => {
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
 
 
+  return (<Layout >
+    <Sidebar />
+    <Layout>
+      {
+        // optional header
+        // <Header></Header>
+      }
+      <Content
+        style={{
+          margin: '24px 16px',
+          padding: 24,
+          minHeight: 400,
+          background: colorBgContainer,
+        }}
+      >
+        <Routes>
+          <Route path="/" Component={Home}></Route>
+          <Route path="/demo" Component={Demo}></Route>
+          <Route path="/components" Component={Components}></Route>
+          <Route path="/portfolio" Component={Portfolio}></Route>
+          <Route path="/projects" Component={Projects}></Route>
+
+          <Route path="*" Component={page404}></Route>
+        </Routes>
+
+      </Content>
+      {
+        // optional header
+        // <Footer><Footer />
+      }
 
 
-  // layout parçala
-  // routing
-  // form
-  // dynamic input
-  // ui/ux
-  // mimari
-  // yetki matrisi  *major
-  // eğitim
-  // performans
-  // sertifikasyon
-  // izin süreçleri
-  // personel alımı
-  // modüler sayfalar
-  // modüler yetkiler
-  // yetki ağaçları
-  // ik 
-  // anket
-  // quiz
-  // görsel
-  // onpremise - cloud ?
-  // küçük ölçek,orta ölçek, büyük ölçek, holding, bir holdşinge bağlı fazla şirket?
-  // çok uzun vadeli özellikler(...)
-  // loglama
+    </Layout>
 
-  // tüm ihtimalleri topla ele ele
+  </Layout>
 
-  // ... only god knows
-
-  return (<Main />)
+  )
 }
 
 
